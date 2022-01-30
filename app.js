f2af40edc688070192ad1ed8333f0d9228362e55
kevanteaniah <kevanteaniah@gmail.com>
2022-01-30 14:34:43 -0500

    Render

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
 
-console.log(
-  "Exercise 9 Result:\n",
-  stringsLongerThan(["say", "goodnight", "whenever", "you", "like"], 3)  
-)
+
 
 const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
-  secretNum: null,
+  secretNum: 7,
   prevGuesses: [],
   play: function() {
+    this.getGuess()
+    let newNum = this.getGuess()
+    
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      if(guess !== this.secretNum){
-        this.prevGuesses.push(guess)
-
+      
+      console.log(this.prevGuesses)
+      console.log(this.secretNum)
+      if(newNum !== this.secretNum){
+        this.prevGuesses.push(newNum)
+        
       }
-      if(guess === this.secretNum){
+      if(newNum === this.secretNum){
+        console.log(this.prevGuesses)
         return `Congrats! You guessed the number in ${prevGuesses.length}!`
       } else{
-        this.prevGuesses.push(guess)
+        this.prevGuesses.push(newNum)
         return `Your guess`
-      }
-game.getGuess
+      } 
+      
+
       
       
       
   },
   getGuess: function(){
-    let guess = 1;
+    let guess
     guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
-    
-  } 
+    console.log(guess)
+  }, 
   
   
 } 
+// console.log(game.play)
+console.log(game.prevGuesses)
+game.play()
 
-
-game.guess
+// while (newNum !== this.secretNum){
+      //   this.prevGuesses.push(newNum){ return `Your guess`
+          
+      //   } (newNum === this.secretNum){
+      //     return `Congrats! You guessed the number in ${prevGuesses.length}!`
+      //   }; 
+        
+      // }
